95a83ee1a295a427c79e612a3000f84246cf128d
parthshrestha <pash4932@colorado.edu>
2023-04-14 11:54:01 -0600

    initialCommit

---
+++ Customer.java
@@ -0,0 +1,10 @@
+import goods.Club;
+
+public class Customer {
+    public String name;
+    int age;
+    int heightCM;
+    Club[] golfBag = new Club[14];
+
+
+}
---
+++ enums/Enums.java
@@ -0,0 +1,7 @@
+package enums;
+
+public class Enums {
+    public enum StaffType {Fitter,Logistic,ServicePerson,Management, SoftGood}
+    public enum Goods {Club,Shaft,Grip,Clothing,Glove,Shoes,Balls,Bag,Accessory,Racket}
+    public enum ClubHeads{Driver,TwoHybrid,ThreeHybrid}
+}
